7bc5b00a912c1fc352da0f6d0dc4d8c4aa8413c2
McChakLeung <441337516@qq.com>
2019-06-28 17:23:45 +0800

    登录功能实现

--- src/main/java/com/dgpalife/resourcemanagement/controller/UserController.java
+++ src/main/java/com/dgpalife/resourcemanagement/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.dgpalife.resourcemanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/login")
-    public String login(){
+    //@Autowired
+    //private UserService userService;
 
+    @RequestMapping("/login")
+    public String login(String username,String password,Model model){
+
+        //userService.selectUserByUserNameAndPassword(username,password);
         return null;
     }
 
---
+++ src/main/java/com/dgpalife/resourcemanagement/service/UserService.java
@@ -0,0 +1,4 @@
+package com.dgpalife.resourcemanagement.service;
+
+public interface UserService {
+}
