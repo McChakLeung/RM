7d357ccf5eb1be4b5ac1fa0356d63155c66816e0
McChakLeung <441337516@qq.com>
2019-09-23 22:50:38 +0800

    建设工单明细展示功能

--- src/main/java/com/dgpalife/resourcemanagement/model/ConstructDetail.java
+++ src/main/java/com/dgpalife/resourcemanagement/model/ConstructDetail.java
@@ -11,19 +11,89 @@ public class ConstructDetail {
 
     private Long expenseId;
 
+    private Expense expense;
+
     private Long workplaceId;
+
+    private Workplace workplace;
 
     private Long networkRoomId;
 
+    private NetworkRoom networkRoom;
+
     private Long projectId;
+
+    private Project project;
 
     private Long orderId;
 
+    private Order order;
+
     private Long templateId;
+
+    private Template template;
 
     private String createTime;
 
     private Long creatorId;
+
+    private User user;
+
+    public Expense getExpense() {
+        return expense;
+    }
+
+    public void setExpense(Expense expense) {
+        this.expense = expense;
+    }
+
+    public Workplace getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(Workplace workplace) {
+        this.workplace = workplace;
+    }
+
+    public NetworkRoom getNetworkRoom() {
+        return networkRoom;
+    }
+
+    public void setNetworkRoom(NetworkRoom networkRoom) {
+        this.networkRoom = networkRoom;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Template getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(Template template) {
+        this.template = template;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public Long getId() {
         return id;
