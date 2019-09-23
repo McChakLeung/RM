c7e7386a6a8660d173a380d42442e65ad8a4b768
McChakLeung <441337516@qq.com>
2019-09-23 20:19:48 +0800

    尝试打jar包

--- src/test/java/com/dgpalife/springbootresourcemanagement/SpringbootResourcemanagementApplicationTests.java
+++ src/test/java/com/dgpalife/springbootresourcemanagement/SpringbootResourcemanagementApplicationTests.java
@@ -1,12 +1,16 @@
 package com.dgpalife.springbootresourcemanagement;
 
+import com.dgpalife.resourcemanagement.SpringbootResourcemanagementApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringbootResourcemanagementApplication.class)
+
+@WebAppConfiguration
 public class SpringbootResourcemanagementApplicationTests {
 
 	@Test
