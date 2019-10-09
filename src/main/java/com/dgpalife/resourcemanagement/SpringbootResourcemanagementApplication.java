7b872d930ca6a5b017c3235dbeb1bf6c7a919008
McChakLeung <441337516@qq.com>
2019-10-09 17:47:11 +0800

    activity与spring boot集成

--- src/main/java/com/dgpalife/resourcemanagement/SpringbootResourcemanagementApplication.java
+++ src/main/java/com/dgpalife/resourcemanagement/SpringbootResourcemanagementApplication.java
@@ -1,12 +1,18 @@
 package com.dgpalife.resourcemanagement;
 
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+//@SpringBootApplication
 @ServletComponentScan
 public class SpringbootResourcemanagementApplication extends SpringBootServletInitializer {
 
